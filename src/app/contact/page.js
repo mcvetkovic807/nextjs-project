7527cb42e0cc1a6ff0ae7ab9a9ac73f3b534413a
Milan Cvetkovic <milan.cvetkovic.807@gmail.com>
2026-01-04 22:58:40 +0100

    06_05b server action

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -1,8 +1,19 @@
 export default function Page() {
+    async function submitForm(formData) {
+        "use server";
+        const formFields = {
+          email: formData.get("email"),
+          message: formData.get("message"),
+        };
+        console.log("formFields", formFields);
+        console.log("TODO: Sebd form field values to backend");
+        return formFields;
+    }
+
     return (
         <main className="max-w-md mx-auto p-6 bg-white shadow-md rounded-md">
             <h1 className="text-2xl font-bold text-center mb-6">Contact</h1>
-            <form className="space-y-4">
+            <form className="space-y-4" action={submitForm}>
                 <div>
                     <label className="block text-sm font-medium text-gray-700" htmlFor="email">Email</label>
                     <input id="email" type="email" name="email" required className="border border-gray-300 focus:ring-2 focus:ring-blue-500 focus:border-blue-500" />
